4d2ce8256bebc501bae70ba38af80647bf9b3616
Mike Ludemann <mike-ludemann@hotmail.de>
2020-02-18 15:53:48 +0100

    Setup complete - vue-radio-button

---
+++ src/components/radio-button.vue
@@ -0,0 +1,64 @@
+<template lang="html">
+
+	<div class="radio-button">
+	<label>
+		<input
+			type="radio"
+			:value="value"
+			:name="name + '[]'"
+			:checked="!!isChecked"
+			@change="e => isChecked = e.target.checked"
+		/>
+		
+	</label>
+	</div>
+
+</template>
+
+<script lang="js">
+
+	export default {
+		name: 'RadioButton',
+		props: {
+			name: {
+				type: string,
+				required: true
+			},
+			value: {
+				type: string,
+				required: true
+			},
+			text: {
+				type: string,
+				required: false
+			},
+			isChecked: {
+				type: boolean,
+				default: false,
+				required: true
+			}
+		},
+		mounted () {
+
+		},
+		data () {
+			return {
+
+			}
+		},
+		methods: {
+
+		},
+		computed: {
+
+		}
+}
+
+
+</script>
+
+<style scoped lang="scss">
+	.radio-button {
+
+	}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <RadioButton name="test" value="tester1" />
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import RadioButton from '@/components/radio-button.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    RadioButton
+  }
+}
+</script>
